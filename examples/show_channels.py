93259ee2acb5d0875f3b0ee3e17ad47da46c8ef0
Dale Evans <dale.evans@mycanadapayday.com>
2020-07-16 17:06:42 -0700

    update print statements to be python3 compatible, clean up * import in examples

--- examples/show_channels.py
+++ examples/show_channels.py
@@ -15,22 +15,21 @@ try:
         # get a status report
         response = manager.status()
         print(response)
-        
+
         response = manager.command('core show channels concise')
         print(response.data)
 
         manager.logoff()
     except asterisk.manager.ManagerSocketException as e:
-        print "Error connecting to the manager: %s" % e.strerror
+        print("Error connecting to the manager: %s" % e.strerror)
         sys.exit(1)
     except asterisk.manager.ManagerAuthException as e:
-        print "Error logging in to the manager: %s" % e.strerror
+        print("Error logging in to the manager: %s" % e.strerror)
         sys.exit(1)
     except asterisk.manager.ManagerException as e:
-        print "Error: %s" % e.strerror
+        print("Error: %s" % e.strerror)
         sys.exit(1)
 
 finally:
     # remember to clean up
     manager.close()
-
